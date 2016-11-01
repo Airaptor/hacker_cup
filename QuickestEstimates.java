907471a97fbb195d6bef9a965d36e4ce190090ff
Thales Ferreira <thales.ferreira@ryerson.ca>
2016-11-01 19:19:30 -0400

    Update QuickestEstimates.java

--- QuickestEstimates.java
+++ QuickestEstimates.java
@@ -1,11 +1,23 @@
-/**
- * Created by Thales Ferreira
- * Github: airaptor
- * Date: 2016-11-01 || 7:09 PM
- */
+import java.util.Scanner;
 
-/**
- * Created by Thales on 2016-11-01.
- */
-public class QuickestEstimates {
+public class QuickEstimate {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N, num;
+        while (in.hasNextInt()){
+            N = in.nextInt();
+            if (N == 0){
+                break;
+            }else {
+                for (int i = 0; i < N; i++) {
+                    num = in.nextInt();
+                    System.out.println(length(num));
+                }
+            }
+        }
+    }
+    public static int length(int num) {
+        int length = (int)(Math.log10(num)+1);
+        return length;
+   }
 }
